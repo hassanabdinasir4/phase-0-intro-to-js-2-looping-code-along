553403effdaacc447a818f6ae5b8bdec1d1ea754
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2022-12-04 08:17:15 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Ada","Brendan","Ali"]
+const eventName = ["surprise"]
+function writeCards(names ,eventName) {
+  let array=[]
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    
+  }
+      return array;
+} 
+
+
+  function countDown(num){
+    while(num >= 0){
+      console.log(num);
+      num--;
+    }
+  }
+    
+
